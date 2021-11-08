15f877b8aa464bcb722b85b5f3160603faa9d499
Kofoed <kofoedsan@gmail.com>
2021-11-08 23:51:01 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import facade from "./apiFacade";
+import LoggedIn from "./LoggedIn";
+import LogIn from "./LogIn";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logout = () => {
+    facade.logout();
+    setLoggedIn(false);
+  };
+
+  const login = (user, pass) => {
+    facade.login(user, pass).then((res) => setLoggedIn(true));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!loggedIn ? (
+        <LogIn login={login} />
+      ) : (
+        <div>
+          <LoggedIn facade={facade} />
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
